6d8c468696a61fbc1d21958bbb6085e97f6c7eb6
Vladislava Balabanova <vladislava.b@ati.su>
2020-01-28 16:23:52 +0300

    Задание 9 для групп

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,2 +1,6 @@
+from model.group import Group
+
 def test_delete_first_group(app):
+    if app.group_helper.count() == 0:
+        app.group_helper.creation(Group(name="test"))
     app.group_helper.delete_first_group()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,9 +1,13 @@
 from model.group import Group
 
 def test_modify_group_name(app):
+    if app.group_helper.count() == 0:
+        app.group_helper.creation(Group(name="test"))
     group = Group(name="New group")
     app.group_helper.modify_first_group(group)
 
 def test_modify_group_header(app):
+    if app.group_helper.count() == 0:
+        app.group_helper.creation(Group(header="headertest"))
     group = Group(header="New Header")
     app.group_helper.modify_first_group(group)
