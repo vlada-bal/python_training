f04ca89c8bfc65183a0f323d71c91ed3041835bc
Vladislava Balabanova <vladislava.b@ati.su>
2020-02-20 00:09:22 +0300

    Задание 17 группы

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,25 +1,15 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata= [Group(name="", header="", footer="")] + [
-    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-    for i in range (5)
-]
 
 #testdata= [Group(name=name, header=header, footer=footer)
  #          for name in ["", random_string("name", 10)]
   #         for header in ["", random_string("header", 20)]
    #        for footer in ["", random_string("footer", 20)]]
 
-@pytest.mark.parametrize ("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, group):
+def test_add_group(app, json_groups):
+    group = json_groups
     old_groups = app.group_helper.get_group_list()
     app.group_helper.creation(group)
     new_groups = app.group_helper.get_group_list()
